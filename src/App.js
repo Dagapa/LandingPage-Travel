00f0e75ecd45cc2ba88865a13a7f581eb681234a
Duvanrozop <rozoparraduvan@gmail.com>
2022-12-24 17:27:18 -0500

    initial librery AOS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+// & css
 import './App.css';
-
-// & Components
-import About from './Components/About/About.jsx';
-import TopGames from './Components/TopGames/TopGames.jsx';
 
 // * Routing
 import { Routes, Route } from 'react-router-dom';
 
+// & Components
+import About from './Components/About/About.jsx';
+import TopGames from './Components/TopGames/TopGames.jsx';
+import CardsGames from './Components/CardsGames/CardsGames.jsx';
+
+// & animations Scroll
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+// * Hooks
+import { useEffect } from 'react';
+
 function App() {
+  // ^ INICIO AOS DE FORMA GLOBAL
+  useEffect(() => {
+    AOS.init({
+      offset: 160,
+    });
+  }, []);
   return (
     <div className='App'>
       <Routes>
@@ -21,7 +36,11 @@ function App() {
           path='/topgames'
           element={<TopGames />}
         />
-        <Route />
+        <Route
+          exact
+          path='/pruebaGamesTemp' // TODO: uso temporal
+          element={<CardsGames />}
+        />
       </Routes>
     </div>
   );
