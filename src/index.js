593f31d833b501e63e69879f6ba7dded4645f72a
Duvanrozop <rozoparraduvan@gmail.com>
2022-12-24 16:53:03 -0500

    add Provider Redux

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -7,15 +6,19 @@ import reportWebVitals from './reportWebVitals';
 // * Routing
 import { BrowserRouter } from 'react-router-dom';
 
+// * Redux
+import { Provider } from 'react-redux';
+import store from './Redux/store/index';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root')
 );
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>
-  </BrowserRouter>
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
