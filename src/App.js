260e5cad0782d8791996b358b7c6745075138e6e
Duvanrozop <rozoparraduvan@gmail.com>
2022-12-25 15:44:33 -0500

    initial dispatch games

--- src/App.js
+++ src/App.js
@@ -1,45 +1,55 @@
-// & css
+// css
 import './App.css';
 
-// * Routing
+// Routing
 import { Routes, Route } from 'react-router-dom';
 
-// & Components
+// Components
 import About from './Components/About/About.jsx';
 import TopGames from './Components/TopGames/TopGames.jsx';
 import CardsGames from './Components/CardsGames/CardsGames.jsx';
 
-// & animations Scroll
+// animations Scroll
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-// * Hooks
+// Hooks
 import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+// actions
+import { getGames, getTopGames } from './Redux/actions/actions';
 
 function App() {
-  // ^ INICIO AOS DE FORMA GLOBAL
+  // start dispatch for games and topGames
+  const dispatch = useDispatch();
+  const getAllGames = () => {
+    dispatch(getGames(dispatch));
+    dispatch(getTopGames(dispatch));
+  };
+
+  // INICIO AOS DE FORMA GLOBAL y hago la peticion de los juegos
   useEffect(() => {
     AOS.init({
-      offset: 160,
+      offset: 130,
     });
+    getAllGames();
   }, []);
+
   return (
     <div className='App'>
       <Routes>
-        <Route
-          exact
-          path='/about'
-          element={<About />}
-        />
-        <Route
-          exact
-          path='/topgames'
-          element={<TopGames />}
-        />
+        <Route exact path='/about' element={<About />} />
+        <Route exact path='/topgames' element={<TopGames />} />
         <Route
           exact
           path='/pruebaGamesTemp' // TODO: uso temporal
           element={<CardsGames />}
+        />
+        <Route
+          exact
+          path='/pruebaTopsGamesTemp' // TODO: uso temporal
+          element={<TopGames />}
         />
       </Routes>
     </div>
