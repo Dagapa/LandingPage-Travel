ecb4d2af61f4d9189f28ed1aeed9bc0ea7bb3903
Duvanrozop <rozoparraduvan@gmail.com>
2022-12-25 22:46:24 -0500

    update components and delete module

--- src/Components/CardsGames/CardsGames.jsx
+++ src/Components/CardsGames/CardsGames.jsx
@@ -1,70 +1,33 @@
 // * componente card
 import CardGame from '../CardGame/CardGame.jsx';
-
+import Loading from '../Loading/Loading.jsx';
 // * Hooks de react redux
 import { connect } from 'react-redux';
-
 // * action de redux
-import { getGames } from '../../Redux/actions/actions.js';
-import { Component, createRef } from 'react';
-
+import { Component } from 'react';
 // * libreria encargada de validar los datos
 import PropTypes from 'prop-types';
-
 // & css
-import styled from './CardsGames.module.css';
-
-// * aminejs
-import anime from 'animejs';
+import './CardsGames.css';
 
 class CardsGames extends Component {
-  state = {
-    isLoading: true,
-  };
+  state = { isLoading: true };
 
-  elementRef = createRef();
-
-  animate = () => {
-    const element = this.elementRef.current;
-
-    anime({
-      targets: element,
-      translateX: [
-        { value: 250, duration: 1000, delay: 500 },
-        { value: -250, duration: 1000, delay: 500 },
-      ],
-      translateY: [
-        { value: -40, duration: 500 },
-        { value: 40, duration: 500, delay: 1000 },
-      ],
-      easing: 'easeOutElastic(1, .8)',
-      loop: true,
-    });
-  };
-
-  componentDidMount() {
-    this.animate();
-    this.props.getAllGames();
+  UNSAFE_componentWillMount() {
+    if (this.props.allGames.length > 0) this.setState({ isLoading: false });
   }
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.allGames !== this.props.allGames)
-      this.setState({ isLoading: false });
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    if (nextProps.allGames.length > 0) this.setState({ isLoading: false });
   }
 
   render() {
-    if (this.state.isLoading) {
-      return (
-        <div className={styled.loadingContainer}>
-          <div ref={this.elementRef} className={styled.box}></div>
-        </div>
-      );
-    }
+    if (this.state.isLoading) return <Loading />;
 
     const gamesPruebas = this.props.allGames.slice(1, 15);
 
     return (
-      <div className={styled.containerGames}>
+      <div className='containerGames'>
         {gamesPruebas.map((game) => {
           return (
             <CardGame
@@ -74,6 +37,7 @@ class CardsGames extends Component {
               title={game.title}
               description={game.short_description}
               genre={game.genre}
+              ruta={'/games'}
             />
           );
         })}
@@ -84,7 +48,6 @@ class CardsGames extends Component {
 
 // * validando los datos que llegan por Props
 CardsGames.propTypes = {
-  getAllGames: PropTypes.func.isRequired,
   allGames: PropTypes.array.isRequired,
 };
 
@@ -93,27 +56,4 @@ const mapStateToProps = (state) => {
   return { allGames: state.games };
 };
 
-// * haciendo el dispatch
-const mapDispatchToProps = (dispatch) => {
-  return {
-    getAllGames: () => {
-      dispatch(getGames(dispatch));
-    },
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(CardsGames);
-
-/*
-this.props.allGames.map((game) => {
-          return (
-            <CardGame
-              key={game.id}
-              img={game.thumbnail}
-              title={game.title}
-              description={game.short_description}
-              genre={game.genre}
-            />
-          );
-        })
-*/
+export default connect(mapStateToProps, null)(CardsGames);
