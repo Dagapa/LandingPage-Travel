40a1a95ad8d7b8ce60535b1745c376801649fa51
Duvanrozop <rozoparraduvan@gmail.com>
2022-12-26 14:34:47 -0500

    renderGames with redux

--- src/Components/CardsGames/CardsGames.jsx
+++ src/Components/CardsGames/CardsGames.jsx
@@ -2,7 +2,7 @@
 import CardGame from '../CardGame/CardGame.jsx';
 import Loading from '../Loading/Loading.jsx';
 // * Hooks de react redux
-import { connect } from 'react-redux';
+import { connect, useSelector } from 'react-redux';
 // * action de redux
 import { Component } from 'react';
 // * libreria encargada de validar los datos
@@ -11,7 +11,7 @@ import PropTypes from 'prop-types';
 import './CardsGames.css';
 
 class CardsGames extends Component {
-  state = { isLoading: true };
+  state = { isLoading: true, gamesRender: [] };
 
   UNSAFE_componentWillMount() {
     if (this.props.allGames.length > 0) this.setState({ isLoading: false });
@@ -21,14 +21,21 @@ class CardsGames extends Component {
     if (nextProps.allGames.length > 0) this.setState({ isLoading: false });
   }
 
+  componentDidUpdate(prevProps) {
+    if (prevProps.render !== this.props.render)
+      this.setState({ ...this.state, gamesRender: this.props.render });
+  }
+
   render() {
     if (this.state.isLoading) return <Loading />;
 
-    const gamesPruebas = this.props.allGames.slice(1, 15);
+    let gamesShow;
+    if (this.state.gamesRender.length > 0) gamesShow = this.state.gamesRender;
+    else gamesShow = this.props.allGames.slice(1, 15);
 
     return (
       <div className='containerGames'>
-        {gamesPruebas.map((game) => {
+        {gamesShow.map((game) => {
           return (
             <CardGame
               key={game.id}
@@ -49,11 +56,12 @@ class CardsGames extends Component {
 // * validando los datos que llegan por Props
 CardsGames.propTypes = {
   allGames: PropTypes.array.isRequired,
+  render: PropTypes.array,
 };
 
 // * obteniendo el estado
 const mapStateToProps = (state) => {
-  return { allGames: state.games };
+  return { allGames: state.games, render: state.renderGames };
 };
 
 export default connect(mapStateToProps, null)(CardsGames);
